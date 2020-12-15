8814dafe2b0872a16bfd7da20ae027975d59447a
Wellington Luiz <wellington.luiz@formalizar.com.br>
2020-12-15 14:50:16 -0300

    feature(configurado serilog para persistencia no mongodb)

--- src/Investimentos.Servico.Api/Program.cs
+++ src/Investimentos.Servico.Api/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Diagnostics;
 
 namespace Investimentos.Servico.Api
 {
@@ -7,11 +11,42 @@ namespace Investimentos.Servico.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                var hostBuilder = CreateHostBuilder(args).Build();
+                Log.Information("Aplicação inicializada com sucesso!");
+                hostBuilder.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Aplicação finalizou de forma inesperada!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+
+                    var configuration = config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true)
+                        .Build();
+
+                    var connectionString = configuration.GetConnectionString("investimentosdb");
+
+                    Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+                    Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.MongoDB(databaseUrl: connectionString, collectionName: "logs")
+                        .CreateLogger();
+                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
